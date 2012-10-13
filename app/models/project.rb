1aa47b3b3ac94dfc4b75ecb00f6d905caf9091fa
Michael Sepcot <msepcot@tunein.com>
2012-10-13 11:35:23 -0700

    add cleaner and validator to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
-  belongs_to :creator, class_name: 'User', foreign_key: :created_by
+  belongs_to :creator,    class_name: 'User', foreign_key: :created_by
+  belongs_to :cleaner,    class_name: 'User', foreign_key: :cleaned_by
+  belongs_to :validator,  class_name: 'User', foreign_key: :validated_by
+
   has_many :photos, as: :owner
 
   scope :open, where(completed_at: nil).order(:id)
