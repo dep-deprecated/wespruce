d5c277109b2aa824e9fae6bc0a31d1d9f4c9b7e1
E.J. Finneran <ej.finneran@gmail.com>
2012-10-12 17:15:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R12Team365::Application.config.secret_token = '176161d36cdd19237d5fa29a764539e3635846804dd9684118392f4f97b7d57d5a4eb9dd7701eb9a5617416e28c364104b61a7c381483080f74c1499442a181c'
