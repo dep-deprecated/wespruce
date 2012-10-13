b8304fc48eeb36b507290aeb691c66f414aa1663
Seth Bro <seth@sethbro.com>
2012-10-13 17:13:26 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seeds = YAML.load_file("#{Rails.root}/db/seeds.yml")
+
+base_lat = 41.9
+base_lng = -87.67
+user_count = User.count
+
+puts seeds.inspect
+seeds['projects'].each do |project|
+  p = Project.new({created_by: User.find(rand(user_count - 1) + 1).id,
+    name: project['name'],
+    description: project['description'],
+    latitude: base_lat + rand(0.05),
+    longitude: base_lng - rand(0.05),
+    rating: (rand(4) + 1)
+  })
+
+  puts p.save
+end
