b62974c573af0db20fb2d3bdf1285f3d8a586e6b
Danny Peck <dannypeck@gmail.com>
2012-10-13 13:14:12 -0400

    Projects table.  Some tweaks to ratings.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,22 @@
 module ApplicationHelper
   def meaning_of_rating rating
-    output = "A #{rating}-star rating means this is a "
-    if rating == 1
-      output << "very easy task that should only take a couple of minutes."
-    elsif rating == 2
-      output << "fairly simple task that should take under an hour."
-    elsif rating == 3
-      output << "somewhat involved task that may take a 2-3 hours."
-    elsif rating == 4
-      output << "pretty difficult task that may take half a day or so."
-    elsif rating == 5
-      output << "very difficult task that will likely take most of the day."
+    if rating > 0
+      output = "A #{rating}-star rating means this is a "
+      if rating == 1
+        output << "very easy task that should only take a couple of minutes."
+      elsif rating == 2
+        output << "fairly simple task that should take under an hour."
+      elsif rating == 3
+        output << "somewhat involved task that may take a 2-3 hours."
+      elsif rating == 4
+        output << "pretty difficult task that may take half a day or so."
+      elsif rating == 5
+        output << "very difficult task that will likely take most of the day."
+      end
+    else
+      output = "A star rating gives you an idea of project complexity. 1 star is a quick task. 5 stars may take all day."
     end
+
     return output.html_safe
   end
 end
