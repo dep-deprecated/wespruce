8dfc4a62d0b3e7e65e14a70c160ce7e4551db124
Michael Sepcot <msepcot@tunein.com>
2012-10-13 12:09:48 -0700

    add AASM to project. rename validator to verifier to match column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_one :photo, as: :owner
   has_many :created_projects,   class_name: 'Project', foreign_key: :created_by
   has_many :cleaned_projects,   class_name: 'Project', foreign_key: :cleaned_by
-  has_many :validated_projects, class_name: 'Project', foreign_key: :validated_by
+  has_many :verified_projects,  class_name: 'Project', foreign_key: :verified_by
 
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
