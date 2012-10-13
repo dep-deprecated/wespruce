1f55a9866f2c74e7f05bccc01c0acc922f6d4cfd
Michael Sepcot <msepcot@tunein.com>
2012-10-13 08:37:10 -0700

    spruce not sprout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ R12Team365::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'wesprout.com' }
+  config.action_mailer.default_url_options = { host: 'wespruce.com' }
 
   # Enable threaded mode
   # config.threadsafe!
