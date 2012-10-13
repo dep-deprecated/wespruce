62282419e8762e5a80582ee3e04c99bc459bfe82
Danny Peck <dannypeck@gmail.com>
2012-10-12 20:49:26 -0400

    Pages controller for generic pages.  New root.

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,8 @@ R12Team365::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  get "pages/:action", to: "pages", action: /[a-z-]+/, as: :page
+  root to: "pages#root"
+
 end
