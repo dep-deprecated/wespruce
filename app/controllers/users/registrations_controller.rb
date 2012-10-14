6e7732ff110a81c58cf34f817a9e546260401d1c
Michael Sepcot <msepcot@tunein.com>
2012-10-13 20:42:37 -0700

    handle projects by zip code, user zip code entry and update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def resource_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :zip_code)
   end
   private :resource_params
+
+protected
+  def after_update_path_for(resource)
+    projects_by_zipcode_path(resource.zip_code)
+  end
 end
